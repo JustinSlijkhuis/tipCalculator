a38792d6c9af72fd834339ac04dd0dcf3d098679
JustinSlijkhuis <justin.slijkhuis@voormeerwaarde.nl>
2022-05-15 13:41:17 +0200

    warning message en karakter limiet, final

--- code/tip.js
+++ code/tip.js
@@ -1,4 +1,4 @@
-/* HTML tag variabeles */
+/* HTML tag variables */
 const total = document.getElementById('total')
 const tipButtons = document.getElementById('tips')
 const tip = document.getElementById('tip')
@@ -6,6 +6,18 @@ const bill = document.getElementById('bill')
 const people = document.getElementById('people')
 const reset = document.getElementById('reset')
 const custom = document.getElementById('custom')
+
+//warnings
+const warningBill = document.getElementById('warning-bill')
+const warningTip = document.getElementById('warning-tip')
+const warningPeople = document.getElementById('warning-people')
+
+
+/* warnings */
+const warn = (type, warning) => {
+    let input = eval("warning" + type)
+    input.innerHTML = warning
+}
 
 
 /* page refresh actions */
@@ -20,12 +32,13 @@ window.onload = function () {
 reset.addEventListener('click', _ => {
     removeBackgroundButton()
     resetValues()
-    resetVariabeles()
+    resetVariables()
     setValues()
+    removeWarnings()
 })
 
 
-/* variabeles */
+/* variables */
 let value = 0
 let valueTip = 0
 let percentage = 0
@@ -33,8 +46,8 @@ let numberOfPeople = 0
 let billValue = 0
 
 
-/* reset the variabeles */
-const resetVariabeles = _ => {
+/* reset the variables */
+const resetVariables = _ => {
     value = 0
     valueTip = 0
     percentage = 0
@@ -54,6 +67,13 @@ const resetValues = _ => {
 }
 
 
+const removeWarnings = _ => {
+    warn("Bill", "")
+    warn("People", "")
+    warn("Tip", "")
+}
+
+
 /* event listeners for input field */
 bill.addEventListener('input', event => {
     value = bill.value
@@ -67,40 +87,14 @@ people.addEventListener('input', event => {
 })
 
 
-const inputLength = 5
 /* check for setting prices/costs */
 const setCosts = id => {
     if(id === "people") {
-        if(isNaN(numberOfPeople) || numberOfPeople === "" || numberOfPeople < 0) {
-            people.classList.remove('succes')
-            if(numberOfPeople === "") {
-                people.classList.remove('warning')
-            } else {
-                people.classList.add('warning')
-            }
-            numberOfPeople = 0
-            setValues()
-        } else {
-            people.classList.remove('warning')
-            people.classList.add('succes')
-            setValues()
-        }
+        securityCheckPeople()
     }
 
     if(id === "bill") {
-        if(isNaN(value) || value === "" || value < 0) {
-            bill.classList.remove('succes')
-            if(value === "") {
-                bill.classList.remove('warning')
-            } else {
-                bill.classList.add('warning')
-            }
-            setValuesZero()
-        } else {
-            bill.classList.remove('warning')
-            bill.classList.add('succes')
-            setValues()
-        }
+        securityCheckBill()
     }
 }
 
@@ -113,12 +107,7 @@ for(let index = 0; index < tipButtons.children.length; index++) {
         ['input', 'click'].forEach(event => tipButton.addEventListener(event, _ => {
             changeButtonBackground(tipButton)
             percentage = getPercentageCustom(tipButton)
-            if(isNaN(tipButton.value) || tipButton.value === "" || percentage < 0) {
-                percentage = 0
-                setValuesZero()
-            } else {
-                setValues()
-            }
+            securityCheckCustom(tipButton)
         }))
     } else {
         /* adds event listener to button */
@@ -129,6 +118,106 @@ for(let index = 0; index < tipButtons.children.length; index++) {
         })
     }
 }
+
+
+
+
+/* security checks */
+const inputLengthBill = 5
+const inputLengthPeople = 2
+const securityCheckCustom = tipButton => {
+    if(isNaN(tipButton.value) || tipButton.value === "" || percentage < 0 || percentage > 100) {
+        if(percentage > 100) {
+            tipButton.value = percentage.slice(0, -1)
+            percentage = tipButton.value
+            setValues()
+            warn("Tip", "Max 100%")
+            return
+        }
+        percentage = 0
+        if(tipButton.value === "") {
+            warn("Tip", "")
+            setValues()
+        } else {
+            warn("Tip", "This is not a valid number!")
+            setValuesZero()
+        }
+    } else {
+        warn("Tip", "")
+        setValues()
+    }
+}
+
+
+
+const securityCheckBill = _ => {
+    if(isNaN(value) || value === "" || value < 1 || value.toString().length > inputLengthBill) {
+        bill.classList.remove('succes')
+        bill.classList.add('warning')
+
+        if(value == 0 && value !== "") {
+            warn("Bill", "Can't be zero")
+            return
+        }
+
+        if(value.toString().length > inputLengthBill) {
+            warn("Bill", `Max ${inputLengthBill} numbers!`)
+            bill.value = bill.value.slice(0, -1)
+            value = bill.value
+            return
+        }
+
+        if(value === "") {
+            bill.classList.remove('warning')
+            warn("Bill", "")
+        } else {
+            warn("Bill", "This is not a valid number!")
+        }
+
+        setValuesZero()
+    } else {
+        bill.classList.remove('warning')
+        bill.classList.add('succes')
+        warn("Bill", "")
+        setValues()
+    }
+}
+
+
+
+const securityCheckPeople = _ => {
+    if(isNaN(numberOfPeople) || numberOfPeople === "" || numberOfPeople < 1 || numberOfPeople.toString().length > inputLengthPeople) {
+        people.classList.remove('succes')
+        people.classList.add('warning')
+
+        if(numberOfPeople == 0 && numberOfPeople !== "") {
+            warn("People", "Can't be zero")
+            return
+        }
+
+        if(numberOfPeople.toString().length > inputLengthPeople) {
+            warn("People", `Max ${inputLengthPeople} numbers!`)
+            people.value = numberOfPeople.slice(0, -1)
+            return
+        }
+
+        if(numberOfPeople === "") {
+            people.classList.remove('warning')
+            warn("People", "")
+        } else {
+            warn("People", "This is not a valid number!")
+        }
+
+        numberOfPeople = 0
+        setValues()
+    } else {
+        people.classList.remove('warning')
+        people.classList.add('succes')
+        warn("People", "")
+        setValues()
+    }
+}
+
 
 
 /* get percentage */
